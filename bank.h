22d558a583ed54dc9dd1b105d2776d0ecc357772
Shravan <nishravan@yahoo.com>
2015-04-14 17:01:46 -0400

    Some erros here

--- bank.h
+++ bank.h
@@ -1,15 +1,20 @@
-typedef struct Account{
-
+typedef struct Account
+{
         char * name;
         float bal;
         int sesFlag;
 
 } acct;
 
-typedef struct Bank{
-
+typedef struct Bank
+{
         acct[20] accounts;
         pthread_mutex_t[20] acmutex;
         pthread_mutex_t_ bankmutex;
 } bank;
 
+void deposit(acct account, float * amount);
+void withdraw(acct account, float * amount);
+void create(char * name, float * bal);
+void serve(char *);
+void query(acct account);
