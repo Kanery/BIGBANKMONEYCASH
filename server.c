47605c7a4541946e9af9392b6a0a33e15b199a70
Daniel Ju <dhj21@cd.cs.rutgers.edu>
2015-04-16 17:07:47 -0400

    Pulling

--- server.c
+++ server.c
@@ -94,12 +94,12 @@ int cyclePrint(int count){
 
 			pthread_mutex_lock(&acMutex[i]);
 
-			printf("Client #%d - %s\n", i+1, myBank->accounts[i].name);
-			printf("Available Balance: %e\n", myBank->accounts[i].bal);
-			if ( myBank->accounts[i].sesFlag == 1)
-				printf("Client %s is currently in session.\n", myBank->accounts[i].name);
+			printf("Client #%d - %s\n", i+1, myBank->accounts[i]->name);
+			printf("Available Balance: %e\n", myBank->accounts[i]->bal);
+			if ( myBank->accounts[i]->sesFlag == 1)
+				printf("Client %s is currently in session.\n", myBank->accounts[i]->name);
 			else
-				printf("Client %s is currently not in session.\n", myBank->accounts[i].name);					
+				printf("Client %s is currently not in session.\n", myBank->accounts[i]->name);					
 
 			pthread_mutex_unlock(&acMutex[i]);
 			
